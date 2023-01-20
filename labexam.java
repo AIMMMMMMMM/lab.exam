c9400a8d79920ba93325df3848ae9c4997bf2d8e
Aimen <122074865+AIMMMMMMMM@users.noreply.github.com>
2023-01-19 20:54:56 -0800

    Update labexam.java

--- labexam.java
+++ labexam.java
@@ -1,4 +1,18 @@
 
 public class labexam {
 
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter an integer >> ");
+      int num = input.nextInt();
+
+      double divisibleByTwo = num % 2;
+
+      if(divisibleByTwo == 0)
+         System.out.println("The integer entered is even.");
+      else
+         System.out.println("The integer entered is odd.");   
+   }
 }
+
